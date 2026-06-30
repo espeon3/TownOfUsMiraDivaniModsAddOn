bb0c0dc2051cfa3c89d871642ba5828934b72041
DivaniNL <dylanvannierop@gmail.com>
2026-06-30 23:12:57 +0200

    Added fix for modifiers

--- Patches/RetributionistNoPostmortemModifierPatch.cs
+++ Patches/RetributionistNoPostmortemModifierPatch.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using HarmonyLib;
 using DivaniMods.Roles.Crewmate.CrewmateKilling;
 using TownOfUs.Modifiers.Game;
@@ -7,10 +8,27 @@ namespace DivaniMods.Patches;
 [HarmonyPatch(typeof(TouGameModifier), nameof(TouGameModifier.IsModifierValidOn))]
 public static class RetributionistNoPostmortemModifierPatch
 {
+    private static readonly PropertyInfo? FactionProperty = typeof(TouGameModifier).GetProperty("FactionType");
+
     [HarmonyPrefix]
     public static bool Prefix(TouGameModifier __instance, RoleBehaviour role, ref bool __result)
     {
-        if (role is RetributionistRole && __instance.FactionType.ToString().Contains("Postmortem"))
+        if (role is not RetributionistRole)
+        {
+            return true;
+        }
+
+        string? faction;
+        try
+        {
+            faction = FactionProperty?.GetValue(__instance)?.ToString();
+        }
+        catch
+        {
+            return true;
+        }
+
+        if (faction != null && faction.Contains("Postmortem"))
         {
             __result = false;
             return false;
