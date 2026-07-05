9b1dca1cfbfb4b99ba9f0baa2efc657b725f1735
DivaniNL <dylanvannierop@gmail.com>
2026-07-05 14:09:04 +0200

    Allowed custom direct kill buttons during rl and tasks

--- Modifiers/WatcherWatchedModifier.cs
+++ Modifiers/WatcherWatchedModifier.cs
@@ -6,11 +6,14 @@ namespace DivaniMods.Modifiers;
 
 public sealed class WatcherWatchedModifier : DisabledModifier
 {
+    public static bool KillButtonContext { get; set; }
+
     public override string ModifierName => "Watched";
     public override bool HideOnUi => true;
     public override LoadableAsset<Sprite>? ModifierIcon => null;
 
-    public override bool CanUseAbilities => false;
+    public override bool CanUseAbilities => KillButtonContext;
+    public override bool CanUseConsoles => true;
     public override bool CanReport => false;
     public override bool CanOpenMap => true;
     public override bool IsConsideredAlive => true;
