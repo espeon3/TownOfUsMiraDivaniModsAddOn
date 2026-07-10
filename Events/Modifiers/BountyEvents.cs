764e517976ca96ce100919e3cb275337353be8fc
espeon3 <espeonicalic2@gmail.com>
2026-07-10 06:44:42 +0200

    create BountyEvents.cs

---
+++ Events/Modifiers/BountyEvents.cs
@@ -0,0 +1,12 @@
+using HarmonyLib;
+using MiraAPI.Events;
+using MiraAPI.Events.Vanilla.Gameplay;
+using MiraAPI.Modifiers;
+using DivaniMods.Modifiers.Game.Crewmate;
+
+namespace DivaniMods.Events.Modifiers;
+
+public static class BountyEvents
+{
+    
+}
