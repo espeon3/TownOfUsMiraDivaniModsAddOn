435170e88319258766cbc4c8fc5f97947261e3ac
espeon3 <espeonicalic2@gmail.com>
2026-07-11 19:59:28 +0200

    Overwork role cs

---
+++ Roles/Crewmate/CrewmatePower/OverworkedRole.cs
@@ -0,0 +1,29 @@
+using Il2CppInterop.Runtime.Attributes;
+using System;
+using System.Collections.Generic;
+using MiraAPI.GameOptions;
+using MiraAPI.Patches.Stubs;
+using MiraAPI.Roles;
+using DivaniMods.Assets;
+using DivaniMods.Options;
+using TownOfUs.Extensions;
+using TownOfUs.Modules.Wiki;
+using TownOfUs.Roles;
+using TownOfUs.Utilities;
+using UnityEngine;
+
+namespace DivaniMods.Roles.Crewmate.CrewmatePower;
+
+public sealed class ThiefRole(IntPtr cppPtr)
+    : CrewmateRole(cppPtr), ITownOfUsRole, IWikiDiscoverable, IDoomable
+{
+    public string RoleName => "Thief";
+    public string RoleDescription => "Task. Overwork. Win.";
+    public string RoleLongDescription => "Do the work for others to bring forth victory for the Crewmates!";
+    public Color RoleColor => new Color(0.5f, 0.3f, 0.1f);
+    public ModdedRoleTeams Team => ModdedRoleTeams.Crewmate;
+    public RoleAlignment RoleAlignment => RoleAlignment.CrewmatePower;
+
+    public DoomableType DoomHintType => DoomableType.Trickster;
+
+    public string GetAdvancedDescription() => RoleLongDescription + MiscUtils.AppendOptionsText(GetType());
