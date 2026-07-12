28ff326c22407fe6c9807d047bb919667f39682b
espeon3 <espeonicalic2@gmail.com>
2026-07-12 07:18:42 +0200

    Tasklist 2

---
+++ Modifiers/Crewmate/CrewmatePower/TasklisttwoModifier.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using AmongUs.GameOptions;
+using MiraAPI.GameOptions;
+using MiraAPI.Modifiers;
+using MiraAPI.Utilities;
+using MiraAPI.Utilities.Assets;
+using DivaniMods.Options;
+using TownOfUs.Assets;
+using TownOfUs.Options.Modifiers.Crewmate;
+using TownOfUs.Roles;
+using TownOfUs.Utilities;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace DivaniMods.Modifiers.Crewmate.CrewmatePower;
+
+public sealed class OverworkedTasklisttwoModifier : BaseModifier
+{
+    public override string ModifierName => "Task list two";
+    public override LoadableAsset<Sprite>? ModifierIcon => Divanimods.Assets;
+    public override bool HideOnUi => true;
